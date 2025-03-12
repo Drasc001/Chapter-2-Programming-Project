cbe59e7f27e34241d7437c63d8eb8017adebc168
Drasc001 <dtr2170@email.vccs.edu>
2025-03-12 15:33:08 -0400

    Commit All

--- Project 2 Distance per tank of gas/Project 2 Distance per tank of gas/Project 2 Distance per tank of gas.cpp
+++ Project 2 Distance per tank of gas/Project 2 Distance per tank of gas/Project 2 Distance per tank of gas.cpp
@@ -15,8 +15,6 @@ int main() {
 	cout << "The distance you can drive on the highway with a 20 gallon gas tank is " << milesonhighway << " miles.\n";
 }
 
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
 
 
 
