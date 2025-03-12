711194dabc9178bbc06bab5fa5ff88c5d8ed4470
Drasc001 <dtr2170@email.vccs.edu>
2025-03-12 15:33:01 -0400

    Answering Project-2 Distance per tank.

--- Project 2 Distance per tank of gas/Project 2 Distance per tank of gas/Project 2 Distance per tank of gas.cpp
+++ Project 2 Distance per tank of gas/Project 2 Distance per tank of gas/Project 2 Distance per tank of gas.cpp
@@ -1,20 +1,22 @@
-// Project 2 Distance per tank of gas.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
+// Calculates the distance per gas tank in town and on the highway for a 20-gallon tank
+
 
 #include <iostream>
+using namespace std;
+int main() {
 
-int main()
-{
-    std::cout << "Hello World!\n";
+	double averagetanksize = 20;
+
+	double milesintown = averagetanksize * 23.5;
+
+	double milesonhighway = averagetanksize * 28.9;
+
+	cout << "The distance you can drive in town with a 20 gallon gas tank is " << milesintown << " miles.\n";
+	cout << "The distance you can drive on the highway with a 20 gallon gas tank is " << milesonhighway << " miles.\n";
 }
 
 // Run program: Ctrl + F5 or Debug > Start Without Debugging menu
 // Debug program: F5 or Debug > Start Debugging menu
 
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
+
+
