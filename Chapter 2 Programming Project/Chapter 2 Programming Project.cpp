edb8d83ba2f26ccd18250c7b192f47ec63b033c1
Drasc001 <dtr2170@email.vccs.edu>
2025-03-12 20:24:06 -0400

    Answering Project 3

--- Chapter 2 Programming Project/Chapter 2 Programming Project.cpp
+++ Chapter 2 Programming Project/Chapter 2 Programming Project.cpp
@@ -1,7 +1,19 @@
-// Chapter 2 Programming Project.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
+// Chapter 2 Programming Project.cpp : The approximate number of customers in the survey purchasing one or more energy drinks per week.
+// The approximate number of customers in the survey preferring citrus - flavored energy drinks.
+
 
 #include <iostream>
+using namespace std;
+
+int main() {
+    double surveyed = 16500; // Correcting the number format
+
+    double PEdrinks = surveyed * 0.15;  // Correct calculation for 15% of customers
+
+    double PreferCitrus = PEdrinks * 0.58;  // 58% of energy drink purchasers prefer citrus
+
+    cout << "Out of 16500 customers approximately " << PEdrinks << " purchased one or more energy drinks per week.\n";
+    cout << "Out of " << PEdrinks << " customers, approximately " << PreferCitrus << " prefer citrus-flavored energy drinks.\n";
 
 int main()
 {
